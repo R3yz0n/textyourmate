a66d2c771a04b95ccdbd041acd0618f48127dc2b
R3yz0n <reyzon.thapa2020@gmail.com>
2024-02-24 22:48:50 +0545

    Update package.json and yarn.lock

---
+++ backend/models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+const User = mongoose.model("User", userSchema);
+export default User;
