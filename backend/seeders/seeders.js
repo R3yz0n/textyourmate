53a7399431e5563e6c5e2344b051c47ff4ca6302
R3yz0n <reyzon.thapa2020@gmail.com>
2024-02-24 22:48:58 +0545

    Add seeders file for data import and deletion

---
+++ backend/seeders/seeders.js
@@ -0,0 +1,39 @@
+import dotenv from "dotenv";
+import colors from "colors";
+import users from "../data/users.js";
+import User from "../models/userModel.js";
+import connectToDB from "../config/db.js";
+
+dotenv.config(process.env.MONGO_URI);
+connectToDB();
+const importData = async () => {
+  try {
+    await User.deleteMany();
+    await User.insertMany(users);
+    // console.log(createdUsers);
+
+    console.log("Data Imported!".green.inverse);
+    process.exit(0);
+  } catch (error) {
+    console.log(error.message.red.inverse);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await User.deleteMany();
+
+    console.log("Data Deleted!".red.inverse);
+    process.exit(0);
+  } catch (error) {
+    console.log(error.message.red.inverse);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === "-d") {
+  destroyData();
+} else {
+  importData();
+}
