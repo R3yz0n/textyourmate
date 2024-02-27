1be7eefc8b435fe4b58628ed2ac732b67d985be2
R3yz0n <reyzon.thapa2020@gmail.com>
2024-02-27 19:47:17 +0545

    Add getAllUsers route and update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/userController.js";
+import { getAllUsers, loginUser, logoutUser, registerUser } from "../controllers/userController.js";
 import { adminVerification, tokenVerification } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(registerUser);
-router.post("/logout", logoutUser);
-router.post("/login", loginUser);
+// router.route("/").post(registerUser);
+// router.post("/logout", logoutUser);
+router.post("/login", loginUser).get("/", tokenVerification, getAllUsers);
 // router.route("/profile").get(tokenVerification, getUserProfile).put(updateUserProfile);
 // router.route("/:id").get(getUserById).put(tokenVerification, adminVerification, updateUser);
 
