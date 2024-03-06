cf1adaa78c7492df1f1b5b3de2a840e0e53d1acf
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-06 16:02:07 +0545

    Add WebSocket connection for real-time messaging

--- frontend/src/redux/services/message/messageApiSlice.ts
+++ frontend/src/redux/services/message/messageApiSlice.ts
@@ -1,4 +1,5 @@
-import { MESSAGE_URL } from "../../../constants";
+import { io } from "socket.io-client";
+import { BASE_URL, MESSAGE_URL } from "../../../constants";
 import apiSlice from "../apiSlice";
 
 export const messageApiSlice = apiSlice
@@ -9,6 +10,34 @@ export const messageApiSlice = apiSlice
         query: (receiverId) => `${MESSAGE_URL}/${receiverId}`,
         //
         providesTags: ["Message"],
+        async onCacheEntryAdded(
+          arg,
+          { updateCachedData, cacheDataLoaded, cacheEntryRemoved, getState }
+        ) {
+          // create a WebSocket connection when the cache subscription starts
+          const { auth } = getState();
+          const socket = io(BASE_URL, {
+            query: {
+              userId: auth?.userInfo?._id,
+            },
+          });
+          console.log("here");
+          socket.on("newMessage", (message) => {
+            console.log(message);
+            updateCachedData((draft) => {
+              draft?.push(message);
+            });
+          });
+
+          try {
+            await cacheDataLoaded;
+          } catch (err) {
+            console.log(err);
+          }
+
+          await cacheEntryRemoved;
+          socket.close();
+        },
       }),
 
       sendMessage: builder.mutation({
