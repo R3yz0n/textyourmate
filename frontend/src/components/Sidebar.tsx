d0cb1075b533258c9566d2b0d752538a8a1cb582
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-12 21:34:50 +0545

    Refactor Sidebar component to include navigation links and logout button

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,15 +1,46 @@
-import SearchInput from "./SearchInput";
+import { useDispatch, useSelector } from "react-redux";
 import Conversations from "./conversation/Conversations";
+import Friends from "./friends/Friends";
+import { Link, NavLink } from "react-router-dom";
+import useCustomDispatch from "../hooks/dispatch";
+import { logout } from "../redux/services/auth/authSlice";
+import { FaUserCircle } from "react-icons/fa";
+import { MdLogout } from "react-icons/md";
+import { IoChatbox } from "react-icons/io5";
+import { FaUserFriends } from "react-icons/fa";
 
 const Sidebar = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
   return (
-    <div className="border-r border-slate-500 p-4 flex flex-col relative">
-      <SearchInput />
-      <div className="divider px-3"></div>
-      <Conversations />
-      {/* <LogoutButton /> */}
-      <div className="bottom-0 absolute cursor-pointer">Logout</div>
-    </div>
+    <nav className=" border-slate-500   flex justify-evenly absolute bottom-0 left-0 w-full py-2    gap-4 bg-black bg-opacity-15 ">
+      <NavLink to="/profile" className={({ isActive }) => (isActive ? "text-cyan-500 " : "")}>
+        <FaUserCircle className="text-2xl" />
+      </NavLink>
+
+      <NavLink to="/" className={({ isActive }) => (isActive ? "text-cyan-500 " : "")}>
+        <p className="items-center flex flex-col text-sm">
+          <IoChatbox className="text-2xl" />
+        </p>
+      </NavLink>
+
+      <NavLink to="/friends" className={({ isActive }) => (isActive ? "text-cyan-500 " : "")}>
+        <p className="items-center flex flex-col text-sm">
+          {" "}
+          <FaUserFriends className="text-2xl" />
+        </p>
+      </NavLink>
+
+      <button
+        className="flex"
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        <MdLogout className="text-2xl" />
+      </button>
+    </nav>
   );
 };
 export default Sidebar;
