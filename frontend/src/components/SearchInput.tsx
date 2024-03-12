e6a1b3c94772034c6a742b1df07bbbec5e0cc267
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-12 21:34:35 +0545

    Update SearchInput component to include an "Add" button and adjust form alignment

--- frontend/src/components/SearchInput.tsx
+++ frontend/src/components/SearchInput.tsx
@@ -2,7 +2,9 @@ import { IoSearchSharp } from "react-icons/io5";
 
 const SearchInput = () => {
   return (
-    <form className="flex items-center gap-2">
+    <form className="flex items-center gap-2 justify-evenly">
+      <button className="btn btn-neutral">Add +</button>
+
       <input type="text" placeholder="Search.." className="input input-bordered rounded-full" />
       <button type="submit" className="btn btn-circle bg-sky-500 text-white">
         <IoSearchSharp />
