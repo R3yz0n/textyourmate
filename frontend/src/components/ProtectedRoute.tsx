c50c04b2550efe99200f10518484d7a94e339f79
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-12 21:34:55 +0545

    Add sidebar and search input to ProtectedRoute component

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,9 +1,22 @@
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import SearchInput from "./SearchInput";
 
 const ProtectedRoute = () => {
   const { userInfo } = useSelector((state: any) => state.auth);
-  return userInfo ? <Outlet /> : <Navigate to={"/login"} replace />;
+
+  if (userInfo)
+    return (
+      <section className="flex flex-col relative h-full">
+        <div className="pt-4">
+          <SearchInput />
+          <Outlet />
+        </div>
+        <Sidebar />
+      </section>
+    );
+  return <Navigate to={"/login"} replace />;
 };
 
 export default ProtectedRoute;
