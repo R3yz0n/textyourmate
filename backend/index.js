e4bcde205b4059550df7def2a313b7c504d2b588
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-12 21:34:41 +0545

    Add conversation routes

--- backend/index.js
+++ backend/index.js
@@ -7,25 +7,27 @@ import { errorHandler, routeNotFound } from "./middleware/errorHandler.js";
 import userRoutes from "./routes/userRoutes.js";
 import connectToDB from "./config/db.js";
 import messageRoutes from "./routes/messageRoute.js";
+import conversationRoutes from "./routes/conversationRoutes.js";
 import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const port = process.env.PORT || 4000;
 // const app = express();
 
-// const corsOptions = {
-//   origin: "http://localhost:5173",
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   credentials: true,
-// };
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/message", messageRoutes);
+app.use("/api/conversation", conversationRoutes);
 app.use(routeNotFound);
 app.use(errorHandler);
 
