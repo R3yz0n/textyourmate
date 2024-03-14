676a7129f4ed897d9b02e12da8dbd6f795d1fefe
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-14 20:29:53 +0545

    Add CONVERSATION_URL constant

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,3 +1,4 @@
 export const BASE_URL = "http://localhost:8000";
 export const USERS_URL = process.env.NODE_ENV === "development" ? "/api/users" : "";
 export const MESSAGE_URL = process.env.NODE_ENV === "development" ? "/api/message" : "";
+export const CONVERSATION_URL = process.env.NODE_ENV === "development" ? "/api/conversation" : "";
