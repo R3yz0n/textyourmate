99fc66c60970c387430f599f38e8aa740e4bf68b
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-14 20:28:01 +0545

    Add getConversationIdByUserId function to conversationController.js

--- backend/controllers/conversationController.js
+++ backend/controllers/conversationController.js
@@ -3,8 +3,21 @@ import Conversation from "../models/conversationModel.js";
 const getAllConversations = asyncHandler(async (req, res) => {
   // Assuming req.user contains information about the currently logged-in user
   const loggedInUserId = req.user._id;
-  const users = await Conversation.find().select(["_id", "lastMessage"]);
+  const users = await Conversation.find({ participants: loggedInUserId })
+    .populate("participants", "name")
+    .select(["_id", "lastMessage"]);
 
   res.status(200).json(users);
 });
-export { getAllConversations };
+
+const getConversationIdByUserId = asyncHandler(async (req, res) => {
+  console.log("test");
+  const loggedInUserId = req.user._id;
+  console.log("test");
+  const conversation = await Conversation.findOne({
+    participants: { $all: [loggedInUserId, req.params.id] },
+  });
+  console.log(conversation);
+});
+
+export { getAllConversations, getConversationIdByUserId };
