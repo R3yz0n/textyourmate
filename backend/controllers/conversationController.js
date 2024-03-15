7f036c853916b2e73991f7e3ad4772b6b37c0405
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-15 20:30:21 +0545

    Refactor conversationController.js to include new functions

--- backend/controllers/conversationController.js
+++ backend/controllers/conversationController.js
@@ -4,20 +4,40 @@ const getAllConversations = asyncHandler(async (req, res) => {
   // Assuming req.user contains information about the currently logged-in user
   const loggedInUserId = req.user._id;
   const users = await Conversation.find({ participants: loggedInUserId })
-    .populate("participants", "name")
+    .select(["_id", "lastMessage"])
+    .populate("participants")
     .select(["_id", "lastMessage"]);
 
+  console.log(users[0]);
   res.status(200).json(users);
 });
 
 const getConversationIdByUserId = asyncHandler(async (req, res) => {
-  console.log("test");
   const loggedInUserId = req.user._id;
-  console.log("test");
   const conversation = await Conversation.findOne({
     participants: { $all: [loggedInUserId, req.params.id] },
   });
   console.log(conversation);
+
+  if (!conversation) {
+    let createdConversation = await Conversation.create({
+      participants: [loggedInUserId, req.params.id],
+    });
+    res.status(200).json(conversation);
+  } else {
+    res.status(200).json(conversation);
+  }
 });
 
-export { getAllConversations, getConversationIdByUserId };
+const getConversationById = asyncHandler(async (req, res) => {
+  const loggedInUserId = req.user._id;
+  const conversation = await Conversation.findOne({
+    _id: req.params.id,
+  });
+  // .populate("messages")
+  // .select(["messages"]);
+  console.log(conversation);
+  res.status(200).json(conversation);
+});
+
+export { getAllConversations, getConversationIdByUserId, getConversationById };
