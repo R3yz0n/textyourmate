0392d17ae7ff59b26af3fff2438098bd74a2125f
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-16 00:50:04 +0545

    Add API status endpoint

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use("/api", () => {
+  console.log("API is online.");
+});
 app.use("/api/users", userRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/conversation", conversationRoutes);
