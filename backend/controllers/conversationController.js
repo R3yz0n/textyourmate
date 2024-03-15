a5588650d0720e474c7cbdc1da045bd8b29a1c8e
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-15 23:58:32 +0545

    Refactor conversationController.js

--- backend/controllers/conversationController.js
+++ backend/controllers/conversationController.js
@@ -13,31 +13,19 @@ const getAllConversations = asyncHandler(async (req, res) => {
 });
 
 const getConversationIdByUserId = asyncHandler(async (req, res) => {
+  console.log(req);
   const loggedInUserId = req.user._id;
   const conversation = await Conversation.findOne({
     participants: { $all: [loggedInUserId, req.params.id] },
   });
-  console.log(conversation);
-
   if (!conversation) {
     let createdConversation = await Conversation.create({
       participants: [loggedInUserId, req.params.id],
     });
-    res.status(200).json(conversation);
+    res.status(200).json(createdConversation);
   } else {
     res.status(200).json(conversation);
   }
 });
 
-const getConversationById = asyncHandler(async (req, res) => {
-  const loggedInUserId = req.user._id;
-  const conversation = await Conversation.findOne({
-    _id: req.params.id,
-  });
-  // .populate("messages")
-  // .select(["messages"]);
-  console.log(conversation);
-  res.status(200).json(conversation);
-});
-
-export { getAllConversations, getConversationIdByUserId, getConversationById };
+export { getAllConversations, getConversationIdByUserId };
