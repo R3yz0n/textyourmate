d79204e824652a5b6dce4b6718bc0e29fb9f0a5b
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-15 23:58:46 +0545

    Refactor conversationRoutes.js to use a single route for getting conversation by friend ID

--- backend/routes/conversationRoutes.js
+++ backend/routes/conversationRoutes.js
@@ -2,17 +2,12 @@ import express from "express";
 import { tokenVerification } from "../middleware/authMiddleware.js";
 import {
   getAllConversations,
-  getConversationById,
   getConversationIdByUserId,
 } from "../controllers/conversationController.js";
 
 const router = express.Router();
 
 router.route("/").get(tokenVerification, getAllConversations);
-
-router
-  .route("/:id")
-  .get(tokenVerification, getConversationById)
-  .post(tokenVerification, getConversationIdByUserId);
+router.get("/friend/:id", tokenVerification, getConversationIdByUserId);
 
 export default router;
