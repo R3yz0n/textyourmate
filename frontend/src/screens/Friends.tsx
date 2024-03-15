689b14ab37545e1c90c63b15d852773ab9ba64dd
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-16 00:18:33 +0545

    Add Friends screen component

---
+++ frontend/src/screens/Friends.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import Friend from "../components/friend/Friend";
+import { useGetAllUsersQuery } from "../redux/services/user/userApiSlice";
+
+const Friends = () => {
+  const { data: users } = useGetAllUsersQuery(undefined); // Pass the required argument
+  return (
+    <div className="py-2 flex flex-col overflow-auto max-h-[400px] ">
+      {users?.map((user) => (
+        <Friend user={user} key={user?._id} />
+      ))}
+    </div>
+  );
+};
+export default Friends;
