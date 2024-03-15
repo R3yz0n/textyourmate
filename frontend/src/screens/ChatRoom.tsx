fdb4c2bb10282e44f644494cad9306d3ded3b221
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-15 23:59:26 +0545

    Refactor ChatRoom component to display messages and implement scroll functionality

--- frontend/src/screens/ChatRoom.tsx
+++ frontend/src/screens/ChatRoom.tsx
@@ -1,13 +1,43 @@
-import React from "react";
 import { useParams } from "react-router-dom";
-import { useGetConversationByIdQuery } from "../redux/services/conversation/conversationApiSlice";
 import { useGetAllMessagesQuery } from "../redux/services/message/messageApiSlice";
+import MessageInput from "../components/messages/MessageInput";
+import Message from "../components/messages/Message";
+import { useLayoutEffect, useRef } from "react";
 
 const ChatRoom = () => {
+  const lastMessageRef = useRef<HTMLDivElement>(null);
   const { id } = useParams();
-  const { data: messages } = useGetAllMessagesQuery(id);
+  const { data } = useGetAllMessagesQuery(id);
+  const messages = data?.messages;
   console.log(messages);
-  return <div>ChatRoom</div>;
+  // console.log(data);
+  const receiver = data?.participants[1];
+  // console.log(receiver);
+
+  useLayoutEffect(() => {
+    // Scroll to the last message when messages change
+    if (lastMessageRef.current) {
+      lastMessageRef.current.scrollIntoView();
+    }
+  }, [messages]);
+  return (
+    <main className="w-full flex flex-col h-full">
+      <>
+        <div className="bg-slate-500 px-4 py-2 mb-2">
+          <span className="label-text ">To:{receiver?.name}</span>
+        </div>
+        <div className="px-4 flex flex-col overflow-auto relative  h-[80%]">
+          {messages?.length > 0 &&
+            messages?.map((message: any, index: number) => (
+              <div key={message._id} ref={index === messages.length - 1 ? lastMessageRef : null}>
+                <Message message={message} />
+              </div>
+            ))}
+        </div>
+        <MessageInput receiverId={receiver?._id} conversationId={id} />
+      </>
+    </main>
+  );
 };
 
 export default ChatRoom;
