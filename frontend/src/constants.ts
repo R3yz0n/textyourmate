2c0b8e2b83749d8e737338a720be2ec9da4e7826
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-16 02:03:43 +0545

    Refactor BASE_URL constant in constants.ts

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,7 +1,9 @@
 // console.log(import.meta.env.VITE_API_URL);
 // console.log(import.meta.env.MODE);
-export const BASE_URL =
-  import.meta.env.MODE === "production" ? import.meta.env.VITE_API_URL : "http://localhost:8000";
+// export const BASE_URL =
+//   import.meta.env.MODE === "development" ? "http://localhost:8000" : import.meta.env.VITE_API_URL;
+export const BASE_URL = import.meta.env.VITE_API_URL;
+console.log(BASE_URL);
 export const USERS_URL = "/api/users";
 export const MESSAGE_URL = "/api/message";
 export const CONVERSATION_URL = "/api/conversation";
