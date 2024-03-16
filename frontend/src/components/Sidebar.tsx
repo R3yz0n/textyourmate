86d82e5db9cf9b4d8a45d30af3bc95445f7dbb9d
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-16 23:17:55 +0545

    Refactor logout functionality in Sidebar component

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,14 +1,20 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
-import { logout } from "../redux/services/auth/authSlice";
+import { logout as clearCredentials } from "../redux/services/auth/authSlice";
 import { FaUserCircle } from "react-icons/fa";
 import { MdLogout } from "react-icons/md";
 import { IoChatbox } from "react-icons/io5";
 import { FaUserFriends } from "react-icons/fa";
+import { useLogoutMutation } from "../redux/services/user/userApiSlice";
 
 const Sidebar = () => {
   const { userInfo } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
+  const [logout] = useLogoutMutation();
+  const handleLogout = async () => {
+    await logout({ mess: "" });
+    dispatch(clearCredentials());
+  };
 
   return (
     <nav className=" border-slate-500   flex justify-evenly absolute bottom-0 left-0 w-full py-2    gap-4 bg-black bg-opacity-15 ">
@@ -29,12 +35,7 @@ const Sidebar = () => {
         </p>
       </NavLink>
 
-      <button
-        className="flex"
-        onClick={() => {
-          dispatch(logout());
-        }}
-      >
+      <button className="flex" onClick={handleLogout}>
         <MdLogout className="text-2xl" />
       </button>
     </nav>
