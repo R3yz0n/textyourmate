ff5551c8a36cfec5eb489f103f9864b1fb25bb5d
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-16 22:45:59 +0545

    Add error handling and display error message in Conversations component

--- frontend/src/screens/Conversations.tsx
+++ frontend/src/screens/Conversations.tsx
@@ -1,12 +1,17 @@
+import { toast } from "react-toastify";
 import Conversation from "../components/conversation/Conversation";
 import { useGetAllConversationQuery } from "../redux/services/conversation/conversationApiSlice";
 
 const Conversations = () => {
-  const { data: conversations } = useGetAllConversationQuery(); // Pass the required argument
+  const { data: conversations, error } = useGetAllConversationQuery(); // Pass the required argument
+  console.log(error);
+
+  let xd = error?.data?.message || error?.message || "An error occurred. Please try again.";
   // console.log(conversations);
   // console.log(conversations);
   return (
     <div className="py-2 flex flex-col overflow-auto h-[65vh] px-2 ">
+      {xd}
       {/* <SearchInput /> */}
       {conversations?.map((conversation) => (
         <Conversation key={conversation._id} conversation={conversation} />
