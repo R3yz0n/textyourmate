a9a708e6def2169b57f5182496fb5aee10cf0e73
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-16 23:30:07 +0545

    Update secure and sameSite options for cookie

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,8 +7,8 @@ const generateToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production" ? true : true,
-    sameSite: process.env.NODE_ENV === "production" ? "strict" : "strict",
+    secure: process.env.NODE_ENV === "production" ? true : false,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000, //30days
   });
 };
