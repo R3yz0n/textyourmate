de00bd5ebe87257c7bb6882f615e20bd37e2304f
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-16 06:41:05 +0545

    Update BASE_URL constant in constants.ts

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,7 +1,8 @@
-export const BASE_URL =
-  import.meta.env.VITE_USER_NODE_ENV === "development"
-    ? "http://localhost:8000"
-    : import.meta.env.VITE_API_URL;
+// export const BASE_URL =
+//   import.meta.env.VITE_USER_NODE_ENV === "development"
+//     ? "http://localhost:8000"
+//     : import.meta.env.VITE_API_URL;
+export const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const USERS_URL = "/api/users";
 export const MESSAGE_URL = "/api/message";
