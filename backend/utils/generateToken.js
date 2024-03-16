af3f80f8ae44d833d2c9de5def838ef3d0d88248
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-16 23:22:08 +0545

    Fix secure and sameSite options in generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production" ? false : false,
+    secure: process.env.NODE_ENV === "production" ? true : true,
     sameSite: process.env.NODE_ENV === "production" ? "strict" : "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000, //30days
   });
