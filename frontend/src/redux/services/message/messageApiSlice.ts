d0fff3d7388244c148e31d7b0ff1b3ca06acbf17
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-18 20:32:53 +0545

    Refactor messageApiSlice to include pagination and cache updates

--- frontend/src/redux/services/message/messageApiSlice.ts
+++ frontend/src/redux/services/message/messageApiSlice.ts
@@ -7,24 +7,49 @@ export const messageApiSlice = apiSlice
   .injectEndpoints({
     endpoints: (builder) => ({
       getAllMessages: builder.query({
-        query: (conversationId) => `${MESSAGE_URL}/${conversationId}`,
-        //
+        query: ({ conversationId, page, limit }) =>
+          `${MESSAGE_URL}/${conversationId}?page=${page}&limit=${limit}`,
+
         providesTags: ["Message"],
+        // transformResponse: (response) => {
+        //   return response;
+        // },
+
+        serializeQueryArgs: ({ queryArgs }) => {
+          const newQueryArgs = { ...queryArgs };
+          if (newQueryArgs.page) {
+            delete newQueryArgs.page;
+          }
+          return newQueryArgs;
+        },
+        merge: (oldCache, newCache) => {
+          // console.log(JSON.stringify(oldCache, null, 2));
+          // console.log(newCache);
+          const { messages, ...rest } = oldCache;
+          console.log(JSON.stringify(rest, null, 2));
+          return {
+            ...newCache.participants,
+            // ...rest,
+            messages: [...newCache.messages, ...oldCache.messages],
+            // ...oldCache,
+          };
+        },
+
         async onCacheEntryAdded(
           arg,
           { updateCachedData, cacheDataLoaded, cacheEntryRemoved, getState }
         ) {
-          // create a WebSocket connection when the cache subscription starts
           const { auth } = getState();
           const socket = io(BASE_URL, {
             query: {
               userId: auth?.userInfo?._id,
             },
           });
+
           socket.on("newMessage", (message) => {
             console.log(message);
             updateCachedData((draft) => {
-              draft?.push(message);
+              draft?.messages?.push(message);
             });
           });
 
@@ -49,12 +74,12 @@ export const messageApiSlice = apiSlice
         // invalidatesTags: ["Message"],
 
         async onQueryStarted(task, { dispatch, queryFulfilled }) {
-          console.log(task);
+          // console.log(task);
           const { data } = await queryFulfilled;
           console.log(data);
           const patchResult = dispatch(
             messageApiSlice.util.updateQueryData("getAllMessages", task.conversationId, (draft) => {
-              console.log(JSON.stringify(draft, null, 2));
+              // console.log(JSON.stringify(draft, null, 2));
               draft?.messages?.push(data);
             })
           );
