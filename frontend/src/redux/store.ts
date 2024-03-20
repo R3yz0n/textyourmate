0ef9fd71bb9ae8f76660d826eaddf83b0bf7a93f
R3yz0n <reyzon.thapa2020@gmail.com>
2024-03-20 18:22:14 +0545

    Add notificationSlice to store

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -3,12 +3,14 @@ import authSlice from "./services/auth/authSlice";
 import userSlice from "./services/user/userSlice";
 import apiSlice from "./services/apiSlice";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import notificationSlice from "./services/notification/notificationSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authSlice,
     user: userSlice,
+    notification: notificationSlice,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
